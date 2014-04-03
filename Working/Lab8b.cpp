18d5d6854f4cee0a8b3298efe59a98f2de72703e
hitchcme <hitchcme@gmail.com>
2014-04-02 21:57:10 -0600

    Hitch working code update 2157

--- Working/Lab8b.cpp
+++ Working/Lab8b.cpp
@@ -1,47 +1,74 @@
-#include<iostream>
-#include<cmath>
-#include<cstring>
+// leftover.cpp -- overloading the left() function
+#include <iostream>
 
-const int ArSize = 80;
+const int ArSize=80;
+long left(long num, int ct);
+char * left(const char * str, int n = 1);
 
-// prototypes
-long left(long num, int ct = 1);
-
-long right(long num, int ct = 1);
-
-char * left(const char * str, int ct = 1);
-
-char * right(const char * str, int n = 1);
-
-int main()	//begin main
+int main()
 {
 	using namespace std;
-
-	char str[ArSize];
-	cout << "Enter a string:\n";
-	cin.getline(str,ArSize);
-
-	long num;
-	cout << "Enter an integer:\n";
-	cin >> num;
+    
+    char str[ArSize];
+    cout<<"Enter a string:\n";
+    cin.getline(str, ArSize);
+    
+    long num;
+    cout<<"Enter an integer:\n";
+	cin>>num;
+    
+	int i;
+	char * temp;
+    
+	for (i = 1; i < 10; i++)
+	{
+		cout << left(num, i) << endl;
+		temp = left(str,i);
+		cout << temp << endl;
+		delete [] temp; // point to temporary storage
+    }
+	return 0;
 }
 
-// This function returns the first ct digits of the number num
-// You are required to modify this function from your lecture notes so that it uses only one while loop: to count the digits.
-// Hint use function pow()
-long left(long num, int ct = 1);
+// This function returns the first ct digits of the number num.
 
-while (long num > 0)
+long left(long num, int ct)
+{
+	int digits = 1;
+	long n = num;
+    
+	if (ct == 0 || num == 0) return 0; // return 0 if no digits
+    
+	while (n /= 10)
+		digits++;
+    
+	if (digits > ct)
+	{
+		ct = digits - ct;
+        
+		while (ct--)
+			num /= 10;
+        
+		return num; // return left ct digits
+	}
+	else return num; // if ct >= number of digits return the whole number
+}
 
+// This function returns a pointer to a new string
+// consisting of the first n characters in the str string.
 
-// This function returns the last ct digits of the number num
-// You are required to code this function so that it uses only one while loop: to count the digits.
-// Hints: use the ideas from the previous function, function pow(), and the modulus operator
-long right(long num, int ct = 1);
+char * left(const char * str, int n)
+{
+	if (n < 0)
+		n = 0;
+    
+	char * p = new char[n+1];
+    
+	int i;
+	for (i = 0; i < n && str[i]; i++)
+		p[i] = str[i]; // copy characters
+    
+	while (i <= n) p[i++] = '\0'; // set rest of string to '\0'
+    return p;
+}
 
-//This function returns a pointer to a new string consisting of the first n characters in the str string
-char * left(const char * str, int ct = 1);
-
-// This function returns a pointer to a new string consisting of the last n characters in the str string
-// Hint: use function strlen()
-char * right(const char * str, int n = 1);
