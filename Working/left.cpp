df92de35e5cfe2d6ddeba28e2e7972b602725608
Rusty74 <rustyshivers@gmail.com>
2014-04-02 21:32:00 -0600

    Update left.cpp

--- Working/left.cpp
+++ Working/left.cpp
@@ -1,39 +1,66 @@
- // left.cpp -- string function with a default argument
-// Hitch's Edit
+// leftover.cpp -- overloading the left() function
 #include <iostream>
-const int ArSize = 80;
 
+long left(long num, int ct);
 char * left(const char * str, int n = 1);
 
 int main()
 {
 	using namespace std;
-	char sample[ArSize];
+	char * trip = "Hawaii!!!"; // test value
+	long n = 123456789; // test value
+	int i;
+	char * temp;
 
-	cout << "Enter a string:\n";
-    cin.get(sample,ArSize);
-
-    char *ps = left(sample, 4);
-    cout << ps << endl;
-    delete [] ps;                  // free old string
-
-    ps = left(sample);
-    cout << ps << endl;
-    delete [] ps;                 // free new string
-    return 0;
+	for (i = 1; i < 10; i++)
+	{
+		cout << left(n, i) << endl;
+		temp = left(trip,i);
+		cout << temp << endl;
+		delete [] temp; // point to temporary storage
+		}
+	return 0;
 }
 
-// This function returns a pointer to a new string // consisting of the first n characters in the str string.
+// This function returns the first ct digits of the number num.
+
+long left(long num, int ct)
+{
+	int digits = 1;
+	long n = num;
+
+	if (ct == 0 || num == 0) return 0; // return 0 if no digits
+
+	while (n /= 10)
+		digits++;
+
+	if (digits > ct)
+	{
+		ct = digits - ct;
+
+		while (ct--)
+			num /= 10;
+
+		return num; // return left ct digits
+	}
+	else return num; // if ct >= number of digits return the whole number
+}
+
+// This function returns a pointer to a new string
+// consisting of the first n characters in the str string.
+
 char * left(const char * str, int n)
 {
 	if (n < 0)
 		n = 0;
-    char * p = new char[n+1];
-    int i;
 
-    for (i = 0; i < n && str[i]; i++)
-    	p[i] = str[i];                               // copy characters
-    while (i <= n)
-    	p[i++] = '\0';                           // set rest of string to '\0‘
-    return p;
+	char * p = new char[n+1];
+
+	int i;
+	for (i = 0; i < n && str[i]; i++)
+		p[i] = str[i]; // copy characters
+
+	while (i <= n) p[i++] = '\0'; // set rest of string to '\0'
+ return p;
 }
+
